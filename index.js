dff149ba10705302d3e2dc9e8388987d39b43b81
TracK92 <40690789+TracK92@users.noreply.github.com>
2022-06-13 20:59:20 +0300

    Update index.js file by adding the mobile options function; Update .github file by adding linters.yml and correcting linters errors

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
+// Open menu bar
+const openMenuBar = () => {
+  const menuBar = document.getElementById('the-menu-bar');
+  const menuBarOverlay = document.getElementById('menu-bar-overlay');
+  menuBar.addEventListener('click', () => {
+    menuBarOverlay.style.display = 'block';
+  });
+};
+openMenuBar();
 
-const openNav = () => {
-    document.getElementById("myNav").style.height = "100%";
-} 
+// Close menu bar
+function closeMenuBar() {
+  document.getElementById('menu-bar-overlay').style.display = 'none';
+}
 
-
-const closeNav = () =>{
-    document.getElementById("myNav").style.height = "0%";
-  }
-
+// Open mobile menu options
+const menuOptions = document.querySelectorAll('.menu-options');
+const mobileMenuOpts = () => {
+  menuOptions.forEach((menuOption) => {
+    menuOption.addEventListener('click', () => {
+      closeMenuBar();
+    });
+  });
+};
+mobileMenuOpts();
