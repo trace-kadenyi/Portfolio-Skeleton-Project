a14ec09932694e1e230099605ee5d119594ed0c1
sireclifford <dev.sire.clifford@gmail.com>
2022-06-17 13:53:19 +0000

    write JS codes to retrieve data from local storage & bind to HTML elements

--- scripts/contact.js
+++ scripts/contact.js
@@ -5,29 +5,41 @@ const clientName = document.querySelector('#myName');
 const clientMessage = document.querySelector('#message');
 const feedbackMessage = document.querySelector('#validationMessage');
 
-const validateEmail = () => {
-  getInTouch.addEventListener('submit', (e) => {
-    const requiredChars = /^[a-z0-9@.]+$/;
-    if (!requiredChars.test(emailInfo.value)) {
-      feedbackMessage.textContent = 'You have provided an invalid email';
-      feedbackMessage.style.color = 'red';
-      feedbackMessage.style.fontWeight = 'bold';
-      e.preventDefault();
-    } else {
-      saveFormData(clientName, emailInfo, clientMessage);
-      feedbackMessage.textContent = 'Success!';
-      feedbackMessage.style.color = 'green';
-      feedbackMessage.style.fontWeight = 'bold';
+const saveFormData = (name, email, message) => {
+    const formData = {
+        userName: name,
+        userEmail: email,
+        userMessage: message,
+    };
+    localStorage.setItem('formData', JSON.stringify(formData));
+};
+
+const getFormData = () => {
+    const formData = JSON.parse(localStorage.getItem('formData'));
+    if (formData) {
+        clientName.value = formData.userName;
+        emailInfo.value = formData.userEmail;
+        clientMessage.value = formData.userMessage;
     }
-  });
+}
+getFormData();
+
+const validateEmail = () => {
+    getInTouch.addEventListener('submit', (e) => {
+        const requiredChars = /^[a-z0-9@.]+$/;
+        if (!requiredChars.test(emailInfo.value)) {
+            feedbackMessage.textContent = 'You have provided an invalid email';
+            feedbackMessage.style.color = 'red';
+            feedbackMessage.style.fontWeight = 'bold';
+            e.preventDefault();
+        } else {
+            saveFormData(clientName.value, emailInfo.value, clientMessage.value);
+            feedbackMessage.textContent = 'Success!';
+            feedbackMessage.style.color = 'green';
+            feedbackMessage.style.fontWeight = 'bold';
+        }
+    });
 };
 validateEmail();
 
-const saveFormData = (name, email, message) => {
-  const formData = {
-    userName: name,
-    userEmail: email,
-    userMessage: message,
-  };
-  localStorage.setItem('formData', JSON.stringify(formData));
-};
+
