94f200ad1dcef0ce8a259113ef0d78eff6e21ea9
TracK92 <40690789+TracK92@users.noreply.github.com>
2022-06-17 17:04:08 +0300

    Update contact.js file by adding a value to the saveFormData function

--- scripts/contact.js
+++ scripts/contact.js
@@ -6,40 +6,38 @@ const clientMessage = document.querySelector('#message');
 const feedbackMessage = document.querySelector('#validationMessage');
 
 const saveFormData = (name, email, message) => {
-    const formData = {
-        userName: name,
-        userEmail: email,
-        userMessage: message,
-    };
-    localStorage.setItem('formData', JSON.stringify(formData));
+  const formData = {
+    userName: name,
+    userEmail: email,
+    userMessage: message,
+  };
+  localStorage.setItem('formData', JSON.stringify(formData));
 };
 
 const getFormData = () => {
-    const formData = JSON.parse(localStorage.getItem('formData'));
-    if (formData) {
-        clientName.value = formData.userName;
-        emailInfo.value = formData.userEmail;
-        clientMessage.value = formData.userMessage;
-    }
-}
+  const formData = JSON.parse(localStorage.getItem('formData'));
+  if (formData) {
+    clientName.value = formData.userName;
+    emailInfo.value = formData.userEmail;
+    clientMessage.value = formData.userMessage;
+  }
+};
 getFormData();
 
 const validateEmail = () => {
-    getInTouch.addEventListener('submit', (e) => {
-        const requiredChars = /^[a-z0-9@.]+$/;
-        if (!requiredChars.test(emailInfo.value)) {
-            feedbackMessage.textContent = 'You have provided an invalid email';
-            feedbackMessage.style.color = 'red';
-            feedbackMessage.style.fontWeight = 'bold';
-            e.preventDefault();
-        } else {
-            saveFormData(clientName, emailInfo, clientMessage);
-            feedbackMessage.textContent = 'Success!';
-            feedbackMessage.style.color = 'green';
-            feedbackMessage.style.fontWeight = 'bold';
-        }
-    });
+  getInTouch.addEventListener('submit', (e) => {
+    const requiredChars = /^[a-z0-9@.]+$/;
+    if (!requiredChars.test(emailInfo.value)) {
+      feedbackMessage.textContent = 'You have provided an invalid email';
+      feedbackMessage.style.color = 'red';
+      feedbackMessage.style.fontWeight = 'bold';
+      e.preventDefault();
+    } else {
+      saveFormData(clientName.value, emailInfo.value, clientMessage.value);
+      feedbackMessage.textContent = 'Success!';
+      feedbackMessage.style.color = 'green';
+      feedbackMessage.style.fontWeight = 'bold';
+    }
+  });
 };
 validateEmail();
-
-
